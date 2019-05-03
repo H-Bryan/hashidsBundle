803fe28ed3babd4834cafe632a104aba835632df
Bryan HAAG <mail@bryanhaag.fr>
2019-05-03 11:43:04 +0200

    Update dependencies

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -5,11 +5,6 @@ namespace cayetanosoriano\HashidsBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
- */
 class Configuration implements ConfigurationInterface
 {
     /**
@@ -17,14 +12,15 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cayetanosoriano_hashids');
-        $rootNode
+        $treeBuilder = new TreeBuilder('cayetanosoriano_hashids');
+        $treeBuilder
+            ->getRootNode()
             ->children()
                 ->scalarNode('salt')->defaultNull()->end()
                 ->scalarNode('min_hash_length')->defaultValue(0)->end()
-                ->scalarNode('alphabet')->defaultValue('')->end()
-                ;
+                ->scalarNode('alphabet')->defaultValue('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')->end()
+            ;
+
         return $treeBuilder;
     }
 }
--- DependencyInjection/cayetanosorianoHashidsExtension.php
+++ DependencyInjection/cayetanosorianoHashidsExtension.php
@@ -3,10 +3,10 @@
 namespace cayetanosoriano\HashidsBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
-
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Definition\Processor;
 /**
  * This is the class that loads and manages your bundle configuration
  *
@@ -19,15 +19,15 @@ class cayetanosorianoHashidsExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $processor = new Processor();
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $config = $processor->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
         $container->setParameter('cayetanosoriano_hashids.salt', $config['salt']);
         $container->setParameter('cayetanosoriano_hashids.min_hash_length', $config['min_hash_length']);
         $container->setParameter('cayetanosoriano_hashids.alphabet', $config['alphabet']);
-        
-
     }
 }
